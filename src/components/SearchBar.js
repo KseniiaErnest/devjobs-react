0c742c55d4a263e19998af52b813354b94ca7ec1
Kseniia Ernest <hramtcovakseniia@gmail.com>
2024-09-03 20:49:32 -0400

    Jobs list display, next load more btn

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function SearchBar() {
   return (
-    <div>
-      
-    </div>
+    <form className='search--container'>
+      <input className='search-input different' />
+      <input className='search-input location' />
+      <label className='search-input check'>
+      <input className='search-input--checkbox'  type='checkbox' />
+      Full Time Only
+      <button className='search--btn'>Search</button>
+      </label>
+    </form>
   )
 }

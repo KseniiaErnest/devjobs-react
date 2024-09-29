705a4a47312dcabc82e4c839c44b9eb7f10164e4
Kseniia Ernest <hramtcovakseniia@gmail.com>
2024-09-29 17:15:33 -0400

    Working on homepage

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -28,8 +28,14 @@ export default function SearchBar() {
 
   return (
     <form onSubmit={handleSubmit} className='search--container'>
+    <div className='input-wrapper'>
+    <svg className='svg-search' width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M17.112 15.059h-1.088l-.377-.377a8.814 8.814 0 002.15-5.784A8.898 8.898 0 008.898 0 8.898 8.898 0 000 8.898a8.898 8.898 0 008.898 8.899c2.211 0 4.23-.808 5.784-2.143l.377.377v1.081l6.845 6.832 2.04-2.04-6.832-6.845zm-8.214 0A6.16 6.16 0 118.9 2.737a6.16 6.16 0 010 12.322z" fill="#5964E0" fill-rule="nonzero"/></svg>
       <input className='search-input different' name='titleCompanyExpertise' value={localSearchQuery.titleCompanyExpertise || ''} onChange={(e) => handleSearchChange('titleCompanyExpertise', e)}  />
+      </div>
+      <div className='input-wrapper'>
+      <svg className='svg-location' width="17" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M14.358 2.451A8.3 8.3 0 008.448 0a8.3 8.3 0 00-5.911 2.451c-2.922 2.925-3.285 8.427-.786 11.76l6.697 9.683 6.687-9.669c2.508-3.347 2.145-8.85-.777-11.774zm-5.833 8.894a3.057 3.057 0 01-3.051-3.054 3.057 3.057 0 013.05-3.055 3.057 3.057 0 013.052 3.055 3.057 3.057 0 01-3.051 3.054z" fill="#5964E0" fill-rule="nonzero"/></svg>
       <input className='search-input location' name='location' value={localSearchQuery.location || ''} onChange={(e) => handleSearchChange('location', e)} />
+      </div>
       <label className='search-input check'>
       <input className='search-input--checkbox'  type='checkbox' name='fullTime' value={localSearchQuery.fullTime || false} onChange={(e) => handleSearchChange('fullTime', e)} />
       Full Time Only
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
 
 export default function Homepage() {
+const [dataRoles, setDataRoles] = useState([]);
+  const [role, setRole] = useState(0);
+
+  const nextRole = () => {
+    setRole((role) => {
+      role++;
+      if (role > dataRoles.length - 1) {
+        role = 0;
+      }
+
+      return role;
+    } )
+  };
+
+  const prevRole = () => {
+    setRole((role) => {
+      role--;
+
+      if (role < 0) {
+        return dataRoles.length - 1;
+      }
+
+      return role;
+    })
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+const response = await fetch('/data-roles.json');
+if (!response.ok) throw new Error('Something is wrong!');
+const data = await response.json();
+setDataRoles(data); 
+      } catch(err) {
+        console.log(err);
+      }
+    }
+    getData();
+  }, [setDataRoles]);
+  
   return (
-    <div>
-      homepage
+    <div className='homepage-container--main'>
+      <div className='homepage-container--welcome'>
+        <h2>Welcome to DevJobs – Your Gateway to the Best Developer Opportunities!</h2>
+        <p>At DevJobs, we connect talented developers with leading companies in the tech industry. Whether you’re a frontend wizard, a backend architect, or a full-stack all-rounder, we have something for everyone. Explore jobs across different roles, find your dream position, and kickstart the next exciting chapter in your tech career.
+
+Let’s get you started – browse the latest openings, apply with ease, and step into your future today!</p>
+      </div>
+
+      <div className='homepage-container--slider'>
+        <button onClick={prevRole}>Prev</button>
+        <div>
+        {dataRoles.length > 0 && (
+          <>
+          <h3>{dataRoles[role].role}</h3>
+          <p>{dataRoles[role].description}</p>
+          <p>IMAGE</p>
+          </>
+        )}
+          
+        </div>
+        <button onClick={nextRole}>Next</button>
+      </div>
     </div>
   )
 }
